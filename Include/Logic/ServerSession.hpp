5d7a3683a7b8275a1f7e84538b80bd2b1f6fae6d
Lars Woxberg <lars.woxberg@gmail.com>
2012-02-28 16:56:28 +0100

    Session setup - protocol added

---
+++ Include/Logic/ServerSession.hpp
@@ -0,0 +1,14 @@
+#ifndef SERVERSESSION_HPP
+#define SERVERSESSION_HPP
+
+#include "Session.hpp"
+
+class ServerSession : public Session
+{
+public:
+
+private:
+
+};
+
+#endif
---
+++ Include/Logic/Session.hpp
@@ -0,0 +1,12 @@
+#ifndef SESSION_HPP
+#define SESSION_HPP
+
+class Session
+{
+public:
+
+private:
+
+};
+
+#endif
